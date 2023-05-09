bc34e94805412f0f46de97372f5d5974c20817de
Sankaku <sankaku_dlt.45631@outlook.jp>
2023-05-09 14:23:21 +0000

    Change game area shape

--- assets/js/game/constants.ts
+++ assets/js/game/constants.ts
@@ -2,7 +2,7 @@ import {TVec2d} from 'curtain-call3';
 
 // size
 export const unit = 256;
-export const gameAreaSize = {x: unit * 6, y: unit * 6};
+export const gameAreaSize = {x: unit * 6, y: unit * 8};
 export const gameAreaSizeHalf = TVec2d.div(gameAreaSize, 2);
 export const gameAreaRect = {nw: TVec2d.mlt(gameAreaSizeHalf, -1), se: gameAreaSizeHalf};
 
