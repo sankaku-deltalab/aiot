2c03667ac41dbf6e2ac7ee73a97a757fd0d0d1ef
Sankaku <sankaku_dlt.45631@outlook.jp>
2023-05-09 13:54:45 +0000

    Fix enemy spawn pos

--- assets/js/game/level-components/enemy-spawner.ts
+++ assets/js/game/level-components/enemy-spawner.ts
@@ -101,9 +101,12 @@ export class TEnemySpawner {
     const stats = DataSourceHelper.fetchB(state, 'enemyStats', 'alpha');
     const collisionSize = TVec2d.mlt({x: stats.collision_size_unit_x, y: stats.collision_size_unit_y}, unit);
     const enemiesAttrs = Enum.map(Im.range(0, spawnCount), () => {
+      const rY = Math.random();
+      const yStart = 0;
+      const yEnd = -(gameAreaSize.y / 2 - unit);
       const spawnPos = {
-        x: (Math.random() - 0.5) * (gameAreaSize.x - unit),
-        y: Math.random() * -0.5 * gameAreaSize.y + unit / 2,
+        x: (Math.random() - 0.5) * (gameAreaSize.x - 2 * unit),
+        y: rY * yEnd + (1 - rY) * yStart,
       };
       return TEnemy.newAttrs({
         pos: spawnPos,
